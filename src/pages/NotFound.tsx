91e67ef96dfd78074407acb10170894240d44a63
Alexey Butkeev <git@abutkeev.ru>
2022-04-22 09:26:52 +0300

    add go home button to 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,19 @@
-import { Alert } from '@mui/material';
+import { Alert, Button } from '@mui/material';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: FC = () => {
+  const navigate = useNavigate();
   return (
-    <Alert severity='error' variant='outlined'>
+    <Alert
+      severity='error'
+      variant='outlined'
+      action={
+        <Button onClick={() => navigate('/')} variant='outlined'>
+          To home
+        </Button>
+      }
+    >
       Page doesn't exist
     </Alert>
   );
